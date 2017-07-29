ce02d5a548be56be103c2ec3939d16c9e5f40e63
limozaza <zakboite@gmail.com>
2017-07-29 09:01:37 +0200

    Create Ressource Place

--- src/AppBundle/Controller/PlaceController.php
+++ src/AppBundle/Controller/PlaceController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Place;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PlaceController extends Controller
@@ -35,6 +36,23 @@ class PlaceController extends Controller
         if(empty($place)){
             return new JsonResponse(['message'=>'Place not found'], Response::HTTP_NOT_FOUND);
         }
+        return $place;
+    }
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post(
+     *     path="/places",
+     *     name="places_add"
+     * )
+     */
+    public function postPlacesAction(Request $request)
+    {
+        $place = new Place();
+        $place->setName($request->get('name'));
+        $place->setAddress($request->get('address'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($place);
+        $em->flush();
         return $place;
     }
 }
